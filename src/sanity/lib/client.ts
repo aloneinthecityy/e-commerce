9b9e6effbeb5404d776c32bb5ba96e876236a353
maria.bastos <maria.bastos@metadados.com.br>
2025-06-08 18:19:14 -0300

    fix: add debug

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -2,6 +2,11 @@ import { createClient } from 'next-sanity'
 
 import { apiVersion, dataset, projectId, token } from '../env'
 
+// Adicione logs para depuração em produção
+console.log("SANITY CLIENT INIT - Project ID:", projectId ? "Loaded" : "MISSING");
+console.log("SANITY CLIENT INIT - Dataset:", dataset ? "Loaded" : "MISSING");
+console.log("SANITY CLIENT INIT - Token:", token ? "Loaded" : "MISSING");
+
 export const client = createClient({
   projectId,
   dataset,
